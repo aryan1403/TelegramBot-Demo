de600620378f59156ce5acd92dfc289de8a81f2c
Aaryan <aaryan14032006@gmail.com>
2021-08-31 16:09:07 +0530

    Initial commit

--- src/main/java/com/example/bot.java
+++ src/main/java/com/example/bot.java
@@ -1,8 +1,8 @@
 package com.example;
 
 import com.example.configurations.cfg;
-
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
@@ -13,18 +13,39 @@ public class bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if(update.getMessage().getText().equals(cfg.handler+"id")) {
+            SendMessage message = new SendMessage(update.getMessage().getChatId().toString(),
+            "id : "+update.getMessage().getChatId().toString());
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    
         // msg = /start
         if (update.getMessage().getText().equals(cfg.handler + "start")) {
-            SendPhoto photo = new SendPhoto(update.getMessage().getChatId().toString(),new InputFile("https://www.internetandtechnologylaw.com/files/2019/06/iStock-872962368-chat-bots.jpg"));
+            SendPhoto photo = new SendPhoto(chatId(update), new InputFile("https://www.internetandtechnologylaw.com/files/2019/06/iStock-872962368-chat-bots.jpg"));
             photo.setCaption("Hi "+update.getMessage().getFrom().getFirstName());
-            try {
+
+            
+            SendDocument document = new SendDocument(chatId(update) , new InputFile("https://www.internetandtechnologylaw.com/files/2019/06/iStock-872962368-chat-bots.jpg"));
+            SendMessage message = new SendMessage(update.getMessage().getChatId().toString(),"Hello");
+
+            try { 
+                execute(message);
                 execute(photo);
+                execute(document);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
 
         }
+    }
 
+    public String chatId(Update update) {
+        return update.getMessage().getChatId().toString();
     }
 
     @Override
