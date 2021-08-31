f30799b15c4048e987b83d052189fcca4f5ba9c9
Aaryan <aaryan14032006@gmail.com>
2021-08-31 15:49:53 +0530

    Added SendPhoto

--- src/main/java/com/example/bot.java
+++ src/main/java/com/example/bot.java
@@ -4,6 +4,8 @@ import com.example.configurations.cfg;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -12,16 +14,17 @@ public class bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         // msg = /start
-        if(update.getMessage().getText().equals(cfg.handler+"start")) {
-            SendMessage message = new SendMessage(update.getMessage().getChatId().toString(), 
-                "Hello "+update.getMessage().getFrom().getFirstName());
+        if (update.getMessage().getText().equals(cfg.handler + "start")) {
+            SendPhoto photo = new SendPhoto(update.getMessage().getChatId().toString(),new InputFile("https://www.internetandtechnologylaw.com/files/2019/06/iStock-872962368-chat-bots.jpg"));
+            photo.setCaption("Hi "+update.getMessage().getFrom().getFirstName());
             try {
-                execute(message);
+                execute(photo);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
+
         }
-        
+
     }
 
     @Override
